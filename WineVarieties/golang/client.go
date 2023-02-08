4562a8161a84c49be7a40f1b9693685f8ffe9c08
mskKandula <mskKandula@gmail.com>
2023-02-08 14:46:51 +0530

    Enhanced as http server

--- WineVarieties/golang/client.go
+++ WineVarieties/golang/client.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/mskKandula/GolangPython/WineVarieties/pb"
 
 	"google.golang.org/grpc"
 )
 
+var client pb.WineClassifierServiceClient
+
 func main() {
-	addr := "localhost:9000"
+	addr := "serverservice:9000"
 	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer conn.Close()
-	client := pb.NewWineClassifierServiceClient(conn)
+	client = pb.NewWineClassifierServiceClient(conn)
+
+	http.HandleFunc("/predict", predictHandler)
+	fmt.Println("listening on port 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+//Handler returns http respone in string format.
+func predictHandler(w http.ResponseWriter, r *http.Request) {
 	req := pb.WineReviewRequest{
 		Review: "Dry with a fruity aroma",
 	}
@@ -25,5 +38,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Fprintf(w, "Predicted wine variety: %v", resp.Variety)
 	log.Printf("Predicted wine variety: %v", resp.Variety)
 }
